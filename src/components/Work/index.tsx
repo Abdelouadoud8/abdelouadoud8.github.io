bc6c3e5ff41987ff5d736b85f22415397862a8e3
Abdelouadoud8 <a.mahdaoui@esi-sba.dz>
2021-10-26 00:30:32 +0100

    Initialisation of fonts and colors scss files

--- src/components/Work/index.tsx
+++ src/components/Work/index.tsx
@@ -1,7 +1,22 @@
 import { FC } from "react";
+import { Col } from "reactstrap";
 
 const Work: FC = () => {
-  return <p>Hello</p>;
+  return (
+    <div>
+      <div className="work__header">
+        <img src="/" alt="" />
+        <h1>Hi, I'm Abdelouadoud</h1>
+        <p>
+          I’m a UI/UX designer & front-end developer focused on building brands
+          and digital experiences - Currently 4th year computer science student
+        </p>
+      </div>
+      <Col md="3">
+        <img src="" alt="" />
+      </Col>
+    </div>
+  );
 };
 
 export default Work;
